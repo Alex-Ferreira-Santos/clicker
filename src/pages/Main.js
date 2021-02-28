0b38b561868796c11d34f6a1404a52098ca46a21
Alex <64551315+Alex-Ferreira-Santos@users.noreply.github.com>
2021-02-28 14:04:35 -0300

    colocando barra de vida

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -4,6 +4,8 @@ import styles from '../styles/main'
 
 function Main(){
     const [coins,setCoins] = useState(0)
+    const [hp, setHp] = useState(10)
+    const [life, setLife] = useState(100)
         return (
             <View style={styles.container}>
                 <View style={styles.coins}>
@@ -16,11 +18,27 @@ function Main(){
                         <Text>10k damage</Text>
                     </View>
                 </View>
+
+                
                 <TouchableHighlight style={styles.clicker} onPress={()=>{
                     setCoins(coins + 1)
+                    setLife(life - 10)
+                    
+                    setHp(hp - 1)
+                    if(hp === 0){
+                        setHp(10)
+                    }
+                    if(life === 0){
+                        setLife(100)
+                    }
                 }} underlayColor='#8AA9B4'>
                     <Text style={styles.buttonText}>click</Text>
                 </TouchableHighlight>
+
+                <View style={styles.health}>
+                    <View style={styles.life}><View style={[styles.TotalLife,{width:`${life}%`}]}/></View>
+                    <Text>{hp} hp</Text>
+                </View>
             </View>
         )
 }
--- src/styles/main.js
+++ src/styles/main.js
@@ -3,7 +3,6 @@ import {StyleSheet} from 'react-native'
 const styles = StyleSheet.create({
     container: {
         flex:1,
-        justifyContent: 'center',
         alignItems: 'center',
         backgroundColor: 'orange',
         width: '100%',
@@ -12,6 +11,7 @@ const styles = StyleSheet.create({
         backgroundColor: 'lightblue',
         padding: 50,
         borderRadius: 10,
+        marginBottom: 20
     },
     buttonText:{
         fontSize: 50
@@ -23,8 +23,26 @@ const styles = StyleSheet.create({
         justifyContent: 'space-around',
         alignItems: 'center',
         borderRadius: 15,
-        position: 'absolute',
-        top: 20
+        marginVertical: 30,
+        
+    },
+    life:{
+        backgroundColor: '#c4c4c4',
+        paddingVertical: 10,
+        width: 200,
+        height: 30,
+        borderRadius: 10,
+        justifyContent: 'center',
+    },
+    health:{
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    TotalLife:{
+        backgroundColor: 'red',
+        paddingVertical: 9,
+        height: 25,
+        borderRadius: 10
     }
 })
 
