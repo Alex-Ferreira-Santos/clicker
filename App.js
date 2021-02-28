903a1b14e2868a60ad082473717aaf667f95f40d
Alex <64551315+Alex-Ferreira-Santos@users.noreply.github.com>
2021-02-28 11:53:41 -0300

    inserindo react navigation

--- App.js
+++ App.js
@@ -1,12 +1,21 @@
 import React,{Component} from 'react';
 import { View, Text} from 'react-native';
+import styles from './src/Styles/app'
+import { NavigationContainer } from '@react-navigation/native'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 
+const Tab = createBottomTabNavigator()
 
 class App extends Component{
   render() {
     return (
-      <View>
+      <View style={styles.container}>
         <Text>Inicio</Text>
+        <NavigationContainer>
+          <Tab.Navigator>
+            <Tab.Screen/>
+          </Tab.Navigator>
+        </NavigationContainer>
       </View>
     )
   }
