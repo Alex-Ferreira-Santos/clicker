14e69f78af24fdc83e18501f08de4e33ce2a45c9
Alex <64551315+Alex-Ferreira-Santos@users.noreply.github.com>
2021-03-01 13:59:35 -0300

    correção de vida de estagio

--- src/context/dataContext.js
+++ src/context/dataContext.js
@@ -1,4 +1,4 @@
-import React,{useState,createContext} from 'react'
+import React,{useState,createContext,useEffect} from 'react'
 
 export const Context = createContext({})
 
@@ -6,22 +6,33 @@ export function DataContext({children}){
     const [times,setTimes] = useState('1x') 
     const [coins,setCoins] = useState(0)
     const [hp, setHp] = useState(10)
+    const [defaultHp, setDefaultHp] = useState(10)
     const [life, setLife] = useState(100)
     const [stage, setStage] = useState(1)
     const [diamonds, setDiamonds] = useState(0)
     const [clickDamage, setClickDamage] = useState(1)
     const [damagePerSecond, setDamagePerSecond] = useState(0)
 
+    /*
+    useEffect(()=>{
+        setLifePercent(hp)
+    },[clickDamage])
+    */
+
     function Click(){
         setCoins(coins + 1)
-        let letLife = life - (clickDamage * 10)
         let letHp = hp - clickDamage
+        let letLife = life - (clickDamage * 10)
+        
         if(letLife <= 0 || letHp <= 0){
             setLife(100)
-            setHp(10)
+            setHp(defaultHp)
             setStage(stage + 1)
             if(stage === 10){
                 setStage(1)
+                let newHp = (defaultHp * 2.5).toFixed(0)
+                setDefaultHp(newHp)
+                setHp(newHp)
             }
         }else{
             setLife(letLife)
