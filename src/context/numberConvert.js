cf6afe1a543d6e18cde77edb42a6a1d2e823fa3d
Alex <64551315+Alex-Ferreira-Santos@users.noreply.github.com>
2021-03-07 14:05:01 -0300

    mudando o toFixed

--- src/context/numberConvert.js
+++ src/context/numberConvert.js
@@ -1,116 +1,201 @@
 export default function numberConvert(number){
-    if(number >= Math.pow(10,90)){
+    if(number >= Math.pow(10,91)){
         number = (number/Math.pow(10,90)).toFixed() + 'AZ'
 
-    }
-    else if(number >= Math.pow(10,87)){
+    }else if(number >= Math.pow(10,90)){
+        number = (number/Math.pow(10,90)).toFixed(1) + 'AZ'
+
+    }else if(number >= Math.pow(10,88)){
         number = (number/Math.pow(10,87)).toFixed() + 'AY'
 
     }
-    else if(number >= Math.pow(10,84)){
+    else if(number >= Math.pow(10,87)){
+        number = (number/Math.pow(10,87)).toFixed(1) + 'AY'
+
+    }else if(number >= Math.pow(10,85)){
         number = (number/Math.pow(10,84)).toFixed() + 'AX'
 
     }
-    else if(number >= Math.pow(10,81)){
+    else if(number >= Math.pow(10,84)){
+        number = (number/Math.pow(10,84)).toFixed(1) + 'AX'
+
+    }else if(number >= Math.pow(10,82)){
         number = (number/Math.pow(10,81)).toFixed() + 'AW'
 
     }
-    else if(number >= Math.pow(10,78)){
+    else if(number >= Math.pow(10,81)){
+        number = (number/Math.pow(10,81)).toFixed(1) + 'AW'
+
+    }else if(number >= Math.pow(10,79)){
         number = (number/Math.pow(10,78)).toFixed() + 'AV'
 
     }
-    else if(number >= Math.pow(10,75)){
+    else if(number >= Math.pow(10,78)){
+        number = (number/Math.pow(10,78)).toFixed(1) + 'AV'
+
+    }else if(number >= Math.pow(10,76)){
         number = (number/Math.pow(10,75)).toFixed() + 'AU'
 
     }
-    else if(number >= Math.pow(10,72)){
+    else if(number >= Math.pow(10,75)){
+        number = (number/Math.pow(10,75)).toFixed(1) + 'AU'
+
+    }else if(number >= Math.pow(10,73)){
         number = (number/Math.pow(10,72)).toFixed() + 'AT'
 
     }
-    else if(number >= Math.pow(10,69)){
+    else if(number >= Math.pow(10,72)){
+        number = (number/Math.pow(10,72)).toFixed(1) + 'AT'
+
+    }else if(number >= Math.pow(10,70)){
         number = (number/Math.pow(10,69)).toFixed() + 'AS'
 
     }
-    else if(number >= Math.pow(10,66)){
+    else if(number >= Math.pow(10,69)){
+        number = (number/Math.pow(10,69)).toFixed(1) + 'AS'
+
+    }else if(number >= Math.pow(10,67)){
         number = (number/Math.pow(10,66)).toFixed() + 'AR'
 
     }
-    else if(number >= Math.pow(10,63)){
+    else if(number >= Math.pow(10,66)){
+        number = (number/Math.pow(10,66)).toFixed(1) + 'AR'
+
+    }else if(number >= Math.pow(10,64)){
         number = (number/Math.pow(10,63)).toFixed() + 'AQ'
 
     }
-    else if(number >= Math.pow(10,60)){
+    else if(number >= Math.pow(10,63)){
+        number = (number/Math.pow(10,63)).toFixed(1) + 'AQ'
+
+    }else if(number >= Math.pow(10,61)){
         number = (number/Math.pow(10,60)).toFixed() + 'AP'
 
     }
-    else if(number >= Math.pow(10,57)){
+    else if(number >= Math.pow(10,60)){
+        number = (number/Math.pow(10,60)).toFixed(1) + 'AP'
+
+    }else if(number >= Math.pow(10,58)){
         number = (number/Math.pow(10,57)).toFixed() + 'AO'
 
     }
-    else if(number >= Math.pow(10,54)){
+    else if(number >= Math.pow(10,57)){
+        number = (number/Math.pow(10,57)).toFixed(1) + 'AO'
+
+    }else if(number >= Math.pow(10,55)){
         number = (number/Math.pow(10,54)).toFixed() + 'AN'
 
     }
-    else if(number >= Math.pow(10,51)){
+    else if(number >= Math.pow(10,54)){
+        number = (number/Math.pow(10,54)).toFixed(1) + 'AN'
+
+    }else if(number >= Math.pow(10,52)){
         number = (number/Math.pow(10,51)).toFixed() + 'AM'
 
     }
-    else if(number >= Math.pow(10,48)){
+    else if(number >= Math.pow(10,51)){
+        number = (number/Math.pow(10,51)).toFixed(1) + 'AM'
+
+    }else if(number >= Math.pow(10,49)){
         number = (number/Math.pow(10,48)).toFixed() + 'AL'
 
     }
-    else if(number >= Math.pow(10,45)){
+    else if(number >= Math.pow(10,48)){
+        number = (number/Math.pow(10,48)).toFixed(1) + 'AL'
+
+    }else if(number >= Math.pow(10,46)){
         number = (number/Math.pow(10,45)).toFixed() + 'AK'
 
     }
-    else if(number >= Math.pow(10,42)){
+    else if(number >= Math.pow(10,45)){
+        number = (number/Math.pow(10,45)).toFixed(1) + 'AK'
+
+    }else if(number >= Math.pow(10,43)){
         number = (number/Math.pow(10,42)).toFixed() + 'AJ'
 
     }
-    else if(number >= Math.pow(10,39)){
+    else if(number >= Math.pow(10,42)){
+        number = (number/Math.pow(10,42)).toFixed(1) + 'AJ'
+
+    }else if(number >= Math.pow(10,40)){
         number = (number/Math.pow(10,39)).toFixed() + 'AI'
 
     }
-    else if(number >= Math.pow(10,36)){
+    else if(number >= Math.pow(10,39)){
+        number = (number/Math.pow(10,39)).toFixed(1) + 'AI'
+
+    }else if(number >= Math.pow(10,37)){
         number = (number/Math.pow(10,36)).toFixed() + 'AH'
 
     }
-    else if(number >= Math.pow(10,33)){
+    else if(number >= Math.pow(10,36)){
+        number = (number/Math.pow(10,36)).toFixed(1) + 'AH'
+
+    }else if(number >= Math.pow(10,34)){
         number = (number/Math.pow(10,33)).toFixed() + 'AG'
 
     }
-    else if(number >= Math.pow(10,30)){
+    else if(number >= Math.pow(10,33)){
+        number = (number/Math.pow(10,33)).toFixed(1) + 'AG'
+
+    }else if(number >= Math.pow(10,31)){
         number = (number/Math.pow(10,30)).toFixed() + 'AF'
 
     }
-    else if(number >= Math.pow(10,27)){
+    else if(number >= Math.pow(10,30)){
+        number = (number/Math.pow(10,30)).toFixed(1) + 'AF'
+
+    }else if(number >= Math.pow(10,28)){
         number = (number/Math.pow(10,27)).toFixed() + 'AE'
 
     }
-    else if(number >= Math.pow(10,24)){
+    else if(number >= Math.pow(10,27)){
+        number = (number/Math.pow(10,27)).toFixed(1) + 'AE'
+
+    }else if(number >= Math.pow(10,25)){
         number = (number/Math.pow(10,24)).toFixed() + 'AD'
 
     }
-    else if(number >= Math.pow(10,21)){
+    else if(number >= Math.pow(10,24)){
+        number = (number/Math.pow(10,24)).toFixed(1) + 'AD'
+
+    }else if(number >= Math.pow(10,22)){
         number = (number/Math.pow(10,21)).toFixed() + 'AC'
 
     }
-    else if(number >= Math.pow(10,18)){
+    else if(number >= Math.pow(10,21)){
+        number = (number/Math.pow(10,21)).toFixed(1) + 'AC'
+
+    }else if(number >= Math.pow(10,19)){
+        
         number = (number/Math.pow(10,18)).toFixed() + 'AB'
     }
-    else if(number >= Math.pow(10,15)){
+    else if(number >= Math.pow(10,18)){
+        number = (number/Math.pow(10,18)).toFixed(1) + 'AB'
+    }
+    else if(number >= Math.pow(10,16)){
         number = (number/Math.pow(10,15)).toFixed() + 'AA'
+    }
+    else if(number >= Math.pow(10,15)){
+        number = (number/Math.pow(10,15)).toFixed(1) + 'AA'
 
+    }else if(number >= Math.pow(10,13)){
+        number = (number/Math.pow(10,12)).toFixed() + 'T'
     }
     else if(number >= Math.pow(10,12)){
-        number = (number/Math.pow(10,12)).toFixed() + 'T'
+        number = (number/Math.pow(10,12)).toFixed(1) + 'T'
 
+    }else if(number >= Math.pow(10,10)){
+        number = (number/Math.pow(10,9)).toFixed() + 'B'
     }
     else if(number >= Math.pow(10,9)){
-        number = (number/Math.pow(10,9)).toFixed() + 'B'
+        number = (number/Math.pow(10,9)).toFixed(1) + 'B'
+
+    }else if(number >= Math.pow(10,7)){
+        number = (number/Math.pow(10,6)).toFixed() + 'M'
 
     }else if(number >= Math.pow(10,6)){
-        number = (number/Math.pow(10,6)).toFixed() + 'M'
+        number = (number/Math.pow(10,6)).toFixed(1) + 'M'
 
     }else if(number >= Math.pow(10,4)){   
         number = (number/Math.pow(10,3)).toFixed() + 'K'   
